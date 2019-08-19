d16bde92eb61e3be70b9b30634c92bb1d78b90b9
kanhaiya <kanhaiyas@lambdatest.com>
2019-08-19 12:11:37 +0530

    change env name to add prefix LT v1

--- test/test.js
+++ test/test.js
@@ -1,13 +1,1 @@
 // TODO: add tests
-fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example`;
-
-test('My first test 1', async t => {
-    await t.typeText('#developer-name', 'John Smith')
-    .click('#submit-button');
-});
-
-fixture`Getting Started 2`.page`http://devexpress.github.io/testcafe/example`;
-test('My first test 2', async t => {
-    await t.typeText('#developer-name', 'John Smith 2')
-    .click('#submit-button');
-});

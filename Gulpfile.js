31107b9f10ba40763de7f699e161fb3b4ffd418d
Juga Paazmaya <paazmaya@yahoo.com>
2019-10-21 16:56:13 +0300

    Gulp structure upgrade

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,6 @@ var gulp        = require('gulp');
 var babel       = require('gulp-babel');
 var mocha       = require('gulp-mocha');
 var del         = require('del');
-var nodeVersion = require('node-version');
 
 
 gulp.task('clean', function () {
@@ -23,14 +22,14 @@ gulp.task('lint', function () {
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('build', ['clean', 'lint'], function () {
+gulp.task('build', gulp.series('clean', 'lint', function () {
     return gulp
         .src('src/**/*.js')
         .pipe(babel())
         .pipe(gulp.dest('lib'));
-});
+}));
 
-gulp.task('test', ['build'], function () {
+gulp.task('test', gulp.series('build', function () {
     return gulp
         .src('test/**.js')
         .pipe(mocha({
@@ -38,4 +37,4 @@ gulp.task('test', ['build'], function () {
             reporter: 'spec',
             timeout:  typeof v8debug === 'undefined' ? 2000 : Infinity // NOTE: disable timeouts in debug
         }));
-});
+}));

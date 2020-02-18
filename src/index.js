f644bd02186e0161381b6ebccd158f628caae855
kanhaiya <kanhaiyas@lambdatest.com>
2020-02-18 18:59:46 +0530

    Fix to added more log for debugging

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ export default {
         
         if (capabilities instanceof Error) {
             showTrace('openBrowser error on  _parseCapabilities', capabilities);
+            await this.dispose()
             throw capabilities;
         }
         await this._startBrowser(id, pageUrl, capabilities);

e24ac9b820a5ab4048faa564c9b095b4e8d8848f
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2021-11-23 17:17:46 +0530

    added changes

--- src/util.js
+++ src/util.js
@@ -15,7 +15,7 @@ const AUTOMATION_BASE_URL = 'https://api.lambdatest.com/automation/api/v1';
 const AUTOMATION_DASHBOARD_URL = 'https://automation.lambdatest.com';
 const AUTOMATION_HUB_URL = process.env.LT_GRID_URL || 'hub.lambdatest.com';
 const LT_AUTH_ERROR = 'Authentication failed. Please assign the correct username and access key to the LT_USERNAME and LT_ACCESS_KEY environment variables.';
-const LT_TUNNEL_NUMBER = process.env.LT_TUNNEL_NUMBER || 5;
+const LT_TUNNEL_NUMBER = process.env.LT_TUNNEL_NUMBER || 1;
 
 var instances = [];
 

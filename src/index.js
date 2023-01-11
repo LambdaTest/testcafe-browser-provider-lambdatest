a4e26af98b1aad609706b99ccd13c6e899affbf5
Shivanshu <95996766+Shivanshu-lambdatest@users.noreply.github.com>
2023-01-11 19:47:50 +0530

    Revert "MLE:12688 Added logic to add enableCustomTranslation for safari>11 and fixed window handles"

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { LT_AUTH_ERROR, PROCESS_ENVIRONMENT, AUTOMATION_DASHBOARD_URL, AUTOMATIO
 
 const WEB_DRIVER_PING_INTERVAL = 30 * 1000;
 
-
 wd.configureHttp({
     timeout: 15 * 60 * 1000,
     
@@ -132,15 +131,13 @@ export default {
 
     // Extra methods
     async resizeWindow (id, width, height) {
-
-
-        const _windowHandle = await this.openedBrowsers[id].windowHandles();
+        const _windowHandle = await this.openedBrowsers[id].windowHandle();
         
         await this.openedBrowsers[id].windowSize(_windowHandle, width, height);
     },
 
     async maximizeWindow (id) {
-        const _windowHandle = await this.openedBrowsers[id].windowHandles();
+        const _windowHandle = await this.openedBrowsers[id].windowHandle();
         
         await this.openedBrowsers[id].maximize(_windowHandle);
     },
